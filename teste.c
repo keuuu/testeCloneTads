a37185f57a6d6b30061d15111bbafb1f517037a1
nathantaques <nathantqs@hotmail.com>
2020-02-14 10:50:52 -0300

    feito

--- teste.c
+++ teste.c
@@ -3,6 +3,19 @@
 
 int main(){
 
+	int num ;
+	
+	printf("digite um numero");
+	scanf("%i" , &num );
+	
+	if (num % 2 == 0 ){
+	
+	printf ("par...");
+	}else{
+	printf("impar..."); 
+	}
+				
+
 
     return 0;
 }
